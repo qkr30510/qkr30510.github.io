8a6f1bc81d6c351b51d0f78016b3cc8d16a16877
juhee <qkr30510@naver.com>
2020-03-10 20:29:00 +0900

    기본 저장 내용 변경

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: `juhee's blog`,
-  description: `Blog posted about ...`,
+  description: `더 나은 미래를 위한 저장소`,
   author: `[박주희]`,
   introduction: `개발자가 되고 싶은 퍼블리셔 이야기.`,
   siteUrl: `https://qkr30510.netlify.com`, // Your blog site url
@@ -14,13 +14,13 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'qkr30510/qkr30510.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: '',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
