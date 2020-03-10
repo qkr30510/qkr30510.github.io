b8f74d3c4cacdde48a3010e82fe20878be33f895
juhee <qkr30510@naver.com>
2020-03-10 20:57:18 +0900

    해더에 연결되는 깃 변경

--- src/components/social-share/github-icon/index.jsx
+++ src/components/social-share/github-icon/index.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 export const GitHubIcon = () => {
   return (
     <a
-      href="https://github.com/JaeYeopHan/felog"
+      href="https://github.com/qkr30510/qkr30510.github.io"
       className="github"
       aria-label="GitHub"
     >
